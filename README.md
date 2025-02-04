82e65b0de2a13bef792f93de5fbbcfb6326610a9
cris0334 <cris0334@gmail.com>
2025-02-04 11:41:32 -0600

    Primera entrega de proyecto. Cambios: - Se agregó toda la lógica de la prueba - Se agregó información para agregar requisitos del pedido - Se actualizó el README.md - Se agregó archivo de requerimientos para ejecutar la prueba

---
+++ README.md
@@ -0,0 +1,43 @@
+# Proyecto Urban Routes 
+
+## _Proyecto para TripleTen QA Sprint 8_
+
+En este proyecto haremos pruebas automatizadas para la aplicación de prueba "Urban Routes", específicamente un uso completo de la funcionalidad de pedir un viaje en taxi con parametros establecidos
+
+Se utilizó lo aprendido en el modulo actual con algo de conocimiento de lo visto en el curso de Data Scientist y leyendo la documentación de https://cnt-00325675-2c3c-4aef-b3b7-805610c2765a.containerhub.tripleten-services.com/docs para realizar las pruebas.
+
+Se utilizó el siguiente IDE para realizar el proyecto y correr las pruebas:
+- PyCharm 2024.3.2 (Community Edition)
+- Build #PC-243.23654.177, built on January 27, 2025
+- Runtime version: 21.0.5+8-b631.30 amd64 (JCEF 122.1.9)
+- VM: OpenJDK 64-Bit Server VM by JetBrains s.r.o.
+- Toolkit: sun.awt.windows.WToolkit
+- Windows 10.0
+- GC: G1 Young Generation, G1 Concurrent GC, G1 Old Generation
+- Memory: 4090M
+- Cores: 8
+- Registry:
+ - ide.experimental.ui=true
+ - llm.show.ai.promotion.window.on.start=false
+- Non-Bundled Plugins:
+ - com.intellij.plugins.vscodekeymap (243.21565.122)
+
+
+Instrucciones para ejecutar las pruebas
+- Tener instalado Python. Al tiempo de creación del proyecto se utilizó la versión 3.12
+- Un entorno base funcional, de preferencia es mejor tener uno exclusivo para el proyecto con conda. Para más información revise la siguiente dirección: https://www.campusmvp.es/recursos/post/como-gestionar-diferentes-entornos-para-python-con-conda.aspx  
+- Instalar las dependencias, para mayor orden se localizan en el archivo requirements.txt. Utilizar el siguiente comando desde la raíz del proyecto: *pip install -r requirements*
+- Instalar pytest: *pip install pytest*
+- En el archivo *configuration.py* cambiar la variable URL_SERVICE por la URL del servidor actual y activo de Urban Routes
+- Ejecutar desde consola, donde "root" es la carpeta base donde se encuentra el proyecto: *pytest root/main.py*
+
+Estructura del proyecto:
+- root
+  - .gitignore: archivos a ignorar en repositorio git
+  - configuration-py: variables de URL para la dirección del servidor de Urban Grocers y las funciones de la API
+  - create_kit_name_kit_test.py: archivo principal de las pruebas 
+  - data.py: archivo con las estructuras de datos para los cuerpos de los mensajes POST de la API y sus respuestas. 
+  - README.md: este archivo
+  - requirements.txt: archivo con las dependencias del proyecto
+  - sender_stand_request.py: archivo con las llamadas a la API de Urban Grocers
+  
--- main.py
+++ main.py
@@ -37,9 +37,37 @@ def retrieve_phone_code(driver) -> str:
 class UrbanRoutesPage:
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
+    phone_number_field = (By.ID, 'phone')
+    add_payment_button = (By.XPATH, "//div[@class='pp-button filled']")
+    card_form_open_button = (By.CLASS_NAME, "pp-plus-container")
+    card_form_close_button = (By.XPATH, '//div[@class="payment-picker open"]//div[@class="section active"]//button[@class="close-button section-close"]')
+    card_number = (By.ID, 'number')
+    card_code = (By.XPATH, "//input[@id='code'][@class='card-input']")
+    phone_code_field = (By.XPATH, '//div[@class="input-container"]//input[@id="code"]')
+
+    # extras_sliders = (By.XPATH, '//input[@class="switch-input"]')
+    extras_sliders = (By.XPATH, "//span[@class='slider round']")
+
+    card_add_button = (By.XPATH, '//div[@class="pp-buttons"]//button[@type="submit"]')
+    icecream_plus_buttons = (By.XPATH, '//div[@class="counter-plus"]')
+    catch_a_ride_button = (By.XPATH, '//div[@class="results-container"]//button')
+    comfort_button = (By.XPATH, '//div[@class="tcard-icon"]//img[@alt="Comfort"]')
+    add_phone_button = (By.CLASS_NAME, 'np-button')
+    add_phone_confirm_button = (By.XPATH, '//button[text()="Confirmar"]')
+    add_phone_send_button = (By.XPATH, '//button[text()="Siguiente"]')
+    order_a_taxi_button = (By.CLASS_NAME, "smart-button")
 
     def __init__(self, driver):
         self.driver = driver
+
+    def wait_for_page_load(self):
+        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(self.from_field))
+
+    def wait_for_payment_page_load(self):
+        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(self.card_form_open_button))
+
+    def wait_for_card_page_load(self):
+        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(self.card_number))
 
     def set_from(self, from_address):
         self.driver.find_element(*self.from_field).send_keys(from_address)
@@ -47,12 +75,94 @@ class UrbanRoutesPage:
     def set_to(self, to_address):
         self.driver.find_element(*self.to_field).send_keys(to_address)
 
+    def set_phone_number(self, phone_number):
+        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(self.phone_number_field))
+        self.driver.find_element(*self.phone_number_field).send_keys(phone_number)
+
     def get_from(self):
         return self.driver.find_element(*self.from_field).get_property('value')
 
     def get_to(self):
         return self.driver.find_element(*self.to_field).get_property('value')
 
+    def get_phone_number(self):
+        return self.driver.find_element(*self.phone_number_field).get_property('value')
+
+    def set_route(self, address_from, address_to):
+        self.set_from(address_from)
+        self.set_to(address_to)
+
+    def select_rate(self, rate):
+        #selected_button = None
+
+        if rate == "comfort":
+            WebDriverWait(self.driver, 5).until(expected_conditions.element_to_be_clickable(self.comfort_button))
+            self.driver.find_element(*self.comfort_button).click()
+            #selected_button = self.driver.find_element(*self.comfort_button)
+
+        #if ~(selected_button is None):
+        #    selected_button.click()
+
+    def set_card_number(self, number):
+        card_number = self.driver.find_element(*self.card_number)
+        card_number.send_keys(number)
+
+    def set_card_code(self, code):
+        card_code = self.driver.find_element(*self.card_code)
+        card_code.send_keys(code + Keys.TAB)
+
+    def set_phone_code(self, phone_code):
+        self.driver.find_element(*self.phone_code_field).send_keys(phone_code)
+
+    def set_card(self, card_number, card_code):
+        self.driver.find_element(*self.add_payment_button).click()
+        self.wait_for_payment_page_load()
+        self.driver.find_element(*self.card_form_open_button).click()
+        self.wait_for_card_page_load()
+        self.set_card_number(card_number)
+        self.set_card_code(card_code)
+        self.driver.find_element(*self.card_add_button).click()
+        self.driver.find_element(*self.card_form_close_button).click()
+
+    def icecream_plus(self, flavor):
+        buttons = self.driver.find_elements(*self.icecream_plus_buttons)
+
+        if flavor == "chocolate":
+            buttons[1].click()
+        elif flavor == "strawberry":
+            buttons[2].click()
+        else:
+            buttons[0].click()
+
+    def set_extras(self, blanket_napkins, acoustic_curtain):
+        extras_sliders = self.driver.find_elements(*self.extras_sliders)
+        if blanket_napkins:
+            extras_sliders[1].click()
+
+        if acoustic_curtain:
+            extras_sliders[2].click()
+
+    def catch_a_ride_click(self):
+        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(self.catch_a_ride_button))
+        self.driver.find_element(*self.catch_a_ride_button).click()
+
+    def add_phone_click(self):
+        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(self.add_phone_button))
+        self.driver.find_element(*self.add_phone_button).click()
+
+    def send_phone_click(self):
+        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(self.add_phone_send_button))
+        self.driver.find_element(*self.add_phone_send_button).click()
+
+    def confirm_phone_click(self):
+        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(self.add_phone_confirm_button))
+        self.driver.find_element(*self.add_phone_confirm_button).click()
+
+    def order_a_taxi_button_click(self):
+        self.driver.find_element(*self.order_a_taxi_button).click()
+
+    def retrieve_phone_code(self):
+        return retrieve_phone_code(self.driver)
 
 
 class TestUrbanRoutes:
@@ -65,17 +175,50 @@ class TestUrbanRoutes:
         from selenium.webdriver import DesiredCapabilities
         capabilities = DesiredCapabilities.CHROME
         capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
-        cls.driver = webdriver.Chrome(desired_capabilities=capabilities)
+        #cls.driver = webdriver.Chrome(desired_capabilities=capabilities)
+        temp_driver = webdriver.Chrome()
+        temp_driver.start_session(capabilities)
+        cls.driver = temp_driver
 
     def test_set_route(self):
         self.driver.get(data.urban_routes_url)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
-        routes_page.set_route(address_from, address_to)
+        rate = data.rate
+        phone_number = data.phone_number
+        card_number = data.card_number
+        card_code = data.card_code
+        icecreams = data.icecreams
+        blanket_napkins = data.blanket_napkins
+        acoustic_curtain = data.acoustic_curtain
+
+        routes_page.wait_for_page_load()
+
+        routes_page.set_route(address_from, address_to) # create the route
+        routes_page.catch_a_ride_click() # click the button to call a taxi
+        routes_page.select_rate(rate) # assign the trip's rate
+
+        routes_page.add_phone_click()
+        routes_page.set_phone_number(phone_number) # assign the phone number
+        routes_page.send_phone_click()
+        phone_code = routes_page.retrieve_phone_code()
+        routes_page.set_phone_code(phone_code)
+        routes_page.confirm_phone_click()
+
+        routes_page.set_card(card_number, card_code) # assign both card number and card code
+
+        routes_page.set_extras(blanket_napkins, acoustic_curtain) # add blanket/napkins
+
+        for flavor in icecreams:
+            for i in range(icecreams[flavor]): # adding icecream buckets
+                routes_page.icecream_plus(flavor)
+
+        routes_page.order_a_taxi_button_click() # ordering the taxi (duda: no se ve el diálogo de espera, ¿cambio algo?)
+
         assert routes_page.get_from() == address_from
         assert routes_page.get_to() == address_to
-
+        assert routes_page.get_phone_number() == phone_number
 
     @classmethod
     def teardown_class(cls):

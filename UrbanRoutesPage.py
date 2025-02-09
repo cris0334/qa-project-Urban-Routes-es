14a93c2d617995a9ae635eb69c0e59871f39440e
cris0334 <cris0334@gmail.com>
2025-02-08 20:17:59 -0600

    Tercera entrega de proyecto. Cambios: - main.py: reestructuración de prueba en multiples pruebas más específicas. - UrbanRoutesPage.py: se agregaron métodos para obtener valores para las nuevas pruebas específicas.

--- UrbanRoutesPage.py
+++ UrbanRoutesPage.py
@@ -7,6 +7,9 @@ from selenium.webdriver.support.wait import WebDriverWait
 class UrbanRoutesPage:
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
+
+    selected_rate = (By.XPATH, '//div[@class="tcard active"]//div[@class="tcard-title"]')
+
     phone_number_field = (By.ID, 'phone')
     add_payment_button = (By.CSS_SELECTOR, "div.pp-button.filled")  # Edit1: Se cambió por CSS_Selector
     card_form_open_button = (By.CLASS_NAME, "pp-plus-container")
@@ -15,11 +18,14 @@ class UrbanRoutesPage:
     card_number = (By.ID, 'number')
     card_code = (By.XPATH, "//input[@id='code'][@class='card-input']")
     phone_code_field = (By.XPATH, '//div[@class="input-container"]//input[@id="code"]')
+    comment_for_driver_field = (By.ID, 'comment')
 
     extras_sliders = (By.XPATH, "//span[@class='slider round']")
+    extras_values = (By.XPATH, '//input[@class="switch-input"]')
 
     card_add_button = (By.XPATH, '//div[@class="pp-buttons"]//button[@type="submit"]')
     icecream_plus_buttons = (By.XPATH, '//div[@class="counter-plus"]')
+    icecream_values = (By.XPATH, '//div[@class="counter-value"]')
     catch_a_ride_button = (By.XPATH, '//div[@class="results-container"]//button')
     comfort_button = (By.XPATH, '//div[@class="tcard-icon"]//img[@alt="Comfort"]')
     add_phone_button = (By.CSS_SELECTOR, 'div.np-button')  # Edit1: Se cambió por CSS_Selector
@@ -56,6 +62,9 @@ class UrbanRoutesPage:
     def get_from(self):
         return self.driver.find_element(*self.from_field).get_property('value')
 
+    def get_selected_rate(self):
+        return self.driver.find_element(*self.selected_rate).get_property('outerText')
+
     def get_to(self):
         return self.driver.find_element(*self.to_field).get_property('value')
 
@@ -70,6 +79,30 @@ class UrbanRoutesPage:
 
     def get_phone_code(self):  # Edit 1: agregar métodos
         return self.driver.find_element(*self.phone_code_field).get_property('value')
+
+    def get_comment_for_driver(self):  # Edit 1: agregar métodos
+        return self.driver.find_element(*self.comment_for_driver_field).get_property('value')
+
+    def get_icecream_by_flavor(self, flavor):
+        icecream_values = self.driver.find_elements(*self.icecream_values)
+
+        return_value = 0
+
+        if flavor == "chocolate":
+            return_value = icecream_values[1].text
+        elif flavor == "strawberry":
+            return_value = icecream_values[2].text
+        else:
+            return_value = icecream_values[0].text
+
+        return int(return_value)
+
+    def get_extras(self):
+        blanket_napkins_and_acoustic_curtain  = self.driver.find_elements(*self.extras_values)
+        blanket_napkins = blanket_napkins_and_acoustic_curtain[0].get_property('checked')
+        acoustic_curtain = blanket_napkins_and_acoustic_curtain[1].get_property('checked')
+
+        return (blanket_napkins, acoustic_curtain)
 
     def set_route(self, address_from, address_to):
         self.set_from(address_from)
@@ -91,17 +124,16 @@ class UrbanRoutesPage:
     def set_card_number(self, card_number):  # Edit 1: agregar métodos, simplifición y assert
         self.driver.find_element(*self.card_number).send_keys(card_number)
 
-        assert self.get_card_number() == card_number
-
     def set_card_code(self, card_code):  # Edit 1: agregar métodos, simplifición y assert
         self.driver.find_element(*self.card_code).send_keys(card_code + Keys.TAB)
-
-        assert self.get_card_code() == card_code
 
     def set_phone_code(self, phone_code):  # Edit 1: assert
         self.driver.find_element(*self.phone_code_field).send_keys(phone_code)
 
         assert self.get_phone_code() == phone_code
+
+    def set_comment_for_driver(self, comment):
+        self.driver.find_element(*self.comment_for_driver_field).send_keys(comment)
 
     def set_card(self, card_number, card_code):
         self.add_payment_button_click()  # Edit 1: cambio por métodos
@@ -110,8 +142,6 @@ class UrbanRoutesPage:
         self.wait_form_card_page_load()  # Edit 1: cambio por métodos
         self.set_card_number(card_number)
         self.set_card_code(card_code)
-        self.card_add_button_click()  # Edit 1: cambio por métodos
-        self.card_form_close_button_click()  # Edit 1: cambio por métodos
 
     def icecream_plus(self, flavor):
         buttons = self.driver.find_elements(*self.icecream_plus_buttons)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 class TestUrbanRoutes:
 
     driver = None
+    routes_page = None
 
     @classmethod
     def setup_class(cls):
@@ -12,48 +13,87 @@ class TestUrbanRoutes:
         from selenium.webdriver import DesiredCapabilities
         capabilities = DesiredCapabilities.CHROME
         capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
-        #cls.driver = webdriver.Chrome(desired_capabilities=capabilities)
         temp_driver = webdriver.Chrome()
         temp_driver.start_session(capabilities)
         cls.driver = temp_driver
+        cls.driver.get(data.urban_routes_url)  # new chrome session
+        cls.routes_page = UrbanRoutesPage(cls.driver)  # open page
 
     def test_set_route(self):
-        self.driver.get(data.urban_routes_url) # new chrome session
-        routes_page = UrbanRoutesPage(self.driver) # open page
-
         # variable assignation
         address_from = data.address_from
         address_to = data.address_to
-        rate = data.rate # comfort rate
-        phone_number = data.phone_number
-        card_number = data.card_number
-        card_code = data.card_code
-        ice_creams = data.ice_creams # Edit 1: ´cambio de nombres de variables
-        blanket_napkins = data.blanket_napkins
-        acoustic_curtain = data.acoustic_curtain
         # end of variable assignation
 
-        routes_page.wait_for_page_load()    # wait for page to open
+        self.routes_page.wait_for_page_load()    # wait for page to open
+        self.routes_page.set_route(address_from, address_to) # create the route
 
-        routes_page.set_route(address_from, address_to) # create the route
-        routes_page.catch_a_ride_click() # click the button to open travel configuration
-        routes_page.select_rate(rate) # assign the trip's rate
-        routes_page.set_phone(phone_number) # assign phone number, get code and use it
+        assert self.routes_page.get_from() == address_from
+        assert self.routes_page.get_to() == address_to
 
-        routes_page.set_card(card_number, card_code) # assign both card number and card code
+    def test_select_plan(self):
+        # variable assignation
+        rate = data.rate  # comfort rate
+        # end of variable assignation
 
-        routes_page.set_extras(blanket_napkins, acoustic_curtain) # add blanket/napkins
+        self.routes_page.catch_a_ride_click() # click the button to open travel configuration
+        self.routes_page.select_rate(rate) # assign the trip's rate
+
+        assert self.routes_page.get_selected_rate().lower() == rate
+
+    def test_fill_phone_number(self):
+        # variable assignation
+        phone_number = data.phone_number
+        # end of variable assignation
+
+        self.routes_page.set_phone(phone_number) # assign phone number, get code and use it
+
+        assert self.routes_page.get_phone_number() == phone_number
+
+    def test_fill_card(self):
+        # variable assignation
+        card_number = data.card_number
+        card_code = data.card_code
+        # end of variable assignation
+
+        self.routes_page.set_card(card_number, card_code) # assign both card number and card code
+
+        assert self.routes_page.get_card_code() == card_code
+        assert self.routes_page.get_card_number() == card_number
+
+        self.routes_page.card_add_button_click()  # Edit 1: cambio por métodos
+        self.routes_page.card_form_close_button_click()  # Edit 1: cambio por métodos
+
+    def test_comment_for_driver(self):
+        # variable assignation
+        message_for_driver = data.message_for_driver
+        # end of variable assignation
+
+        self.routes_page.set_comment_for_driver(message_for_driver)
+
+        assert self.routes_page.get_comment_for_driver() == message_for_driver
+
+    def test_order_extras(self):
+        blanket_napkins = data.blanket_napkins
+        acoustic_curtain = data.acoustic_curtain
+
+        self.routes_page.set_extras(blanket_napkins, acoustic_curtain)  # add blanket/napkins
+
+        assert self.routes_page.get_extras() == (blanket_napkins, acoustic_curtain)
+
+    def test_order_ice_creams(self):
+        ice_creams = data.ice_creams  # Edit 1: cambio de nombres de variables
 
         for flavor in ice_creams: # adding icecream buckets
             for i in range(ice_creams[flavor]):
-                routes_page.icecream_plus(flavor)
+                self.routes_page.icecream_plus(flavor)
 
-        routes_page.order_a_taxi_button_click() # ordering the taxi (duda: no se ve el diálogo de espera, ¿cambio algo?)
+            assert self.routes_page.get_icecream_by_flavor(flavor) == ice_creams[flavor]
 
-        # Edit 1: asserts fueron movidos a sus métodos 
-        #assert routes_page.get_from() == address_from
-        #assert routes_page.get_to() == address_to
-        #assert routes_page.get_phone_number() == phone_number
+    def test_car_search_model_appears(self):
+        self.routes_page.order_a_taxi_button_click() # ordering the taxi
+
+        # No puedo hacer el assert porque no se mantiene el
 
     @classmethod
     def teardown_class(cls):
